9177814341c38485ea0763cfb4d33ab90bf02a6b
Mark <kd2qar@gmail.com>
2025-05-07 05:58:10 -0400

    update sql

--- archive_and_delete_varac_alerts.sql
+++ archive_and_delete_varac_alerts.sql
@@ -13,11 +13,15 @@ Folder IDs:
 	2: Archived
 	3: Sent
 
-Note: This works with VARAC database version 25 YMMV
+Note: This works with VARAC database versions 18-25 YMMV
 
 Use this at your own risk
 ** Wear a Helmet **
 */
+
+SELECT IIF( ABS(parameter_value) >= 18 AND ABS(parameter_value) <= 25,'YES','NO'+ ABS(-9223372036854775808)) AS `Validated Version` 
+		FROM parameter WHERE parameter_name = 'db_version' LIMIT 1; -- Intentionally throws an error if version is out of range
+
 UPDATE alert SET is_deleted = 1, read_status = 1, folder_id = 2 WHERE folder_id = 1;
 UPDATE alert SET is_deleted = 1 WHERE folder_id = 3;
 UPDATE alert SET is_deleted = 1, read_status = 1 WHERE folder_id = 2 AND is_deleted = 0;
